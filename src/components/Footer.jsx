4b5392156925155fb773419e39167e3844297a1f
unknown <sahatmarbun147@gmail.com>
2022-02-23 15:24:41 +0700

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row" className="m-0">
+
+                    <div className="social-media">
+                        <div className="box-icon">
+                            <a href="https://www.twitter.com/"><i class="bi bi-twitter"></i></a>
+                        </div>
+                        <div className="box-icon">
+                            <a href="https://www.linkedin.com/"><i class="bi bi-linkedin"></i></a>
+                        </div>
+                        <div className="box-icon">
+                            <a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>
+                        </div>
+                    </div>
+                    <div className="nav-footer">
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link href="#">About</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#">For Business</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#">Suggestion</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#">Help & FAQs</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#">Contact</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#">Pricing</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </div>
+
+                    <div className="copy-right">
+                        <div className="copy">&copy; 2020 Social Network. All Rights Reserved.</div>
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link href="#">Privacy</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#">Terms</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
